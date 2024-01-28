60cc5027999be311438eaef00a286ff764e917a1
milton munoz <milton.munozr@outlook.com>
2024-01-28 18:42:48 -0500

    feature hibernatePrimaryKeys with UUID 4122

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -2,8 +2,10 @@ package guru.springframework.sdjpaintro.bootstrap;
 
 import guru.springframework.sdjpaintro.domain.AuthorUuid;
 import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.domain.BookUuid;
 import guru.springframework.sdjpaintro.repositories.AuthorUuidRepository;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
+import guru.springframework.sdjpaintro.repositories.BookUuidRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -16,9 +18,12 @@ public class DataInitializer implements CommandLineRunner {
     private final BookRepository bookRepository;
     private final AuthorUuidRepository authorUuidRepository;
 
-    public DataInitializer(BookRepository bookRepository, AuthorUuidRepository authorUuidRepository) {
+    private final BookUuidRepository bookUuidRepository;
+
+    public DataInitializer(BookRepository bookRepository, AuthorUuidRepository authorUuidRepository, BookUuidRepository bookUuidRepository) {
         this.bookRepository = bookRepository;
         this.authorUuidRepository= authorUuidRepository;
+        this.bookUuidRepository=bookUuidRepository;
     }
 
     //implementar los metodos del CommandLineRunner
@@ -45,12 +50,18 @@ public class DataInitializer implements CommandLineRunner {
             System.out.println("Book Title: "+book.getTitle());
 
         });
-
+        //For Author UUID example
         AuthorUuid authorUuid = new AuthorUuid();
         authorUuid.setFirstName("Joe");
         authorUuid.setLastName("Buck");
         AuthorUuid savedAuthor = authorUuidRepository.save(authorUuid);
         System.out.println("Saved Author UUID: " + savedAuthor.getId());
+
+        //FOr Book UUID example
+        BookUuid bookUuid = new BookUuid();
+        bookUuid.setTitle("All about UUIDs");
+        BookUuid savedBookUuid = bookUuidRepository.save(bookUuid);
+        System.out.println("saved Book UUID: "+ savedBookUuid.getId());
 
     }
 }
